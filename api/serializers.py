c13b1fe7c1ed7c4891f6dbb51a9ed9fd5d99ad16
Joe <1006953367@qq.com>
2019-03-20 19:52:57 +0800

    improve serializers with atrributes, methods, and override mothod

--- api/serializers.py
+++ api/serializers.py
@@ -1,16 +1,18 @@
+from collections import OrderedDict
+
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from todo import models
 
 
-class TodoSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = (
-            'id',
-            'title',
-            'description',
-        )
-        model = models.Todo
+# class TodoSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         fields = (
+#             'id',
+#             'title',
+#             'description',
+#         )
+#         model = models.Todo
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -21,3 +23,45 @@ class UserSerializer(serializers.ModelSerializer):
             'password'
         )
         model = User
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    # rewrite the represent of attributes
+    todo_id = serializers.IntegerField(source='id')
+    todo_title = serializers.CharField(source='title')
+    todo_description = serializers.CharField(source='description')
+
+    writer = serializers.SerializerMethodField()
+
+    class Meta:
+        # fields = '__all__'
+        fields = (
+            'todo_id',
+            'todo_title',
+            'todo_description',
+            'writer',
+        )
+        model = models.Todo
+
+    # def get_roleId(self, obj):
+    #     user_id = obj.id
+    #     user_to_role = UserToRole.objects.filter(user_id=user_id).first()
+    #     return user_to_role.role_id if user_to_role else None
+
+    # def get_rolename(self, obj):
+    #     role_id = self.get_roleId(obj)
+    #     return Role.objects.filter(id=role_id).first().name if role_id else ''
+
+    # custom attribute, especially from a unrelated table
+    def get_writer(self, obj):
+        name = obj.title
+        writer = User.objects.filter(username=name).first()
+        # can also get the multiple data from another XxxSerializer()
+        # >>> return UserSerializer(writer, many=True).data
+        # notice: param 'writer' needs to be a <QuerySet> object, which is iterable
+        return writer.username if writer else None
+
+    # remove the None field from the result
+    def to_representation(self, instance):
+        result = super(TodoSerializer, self).to_representation(instance)
+        return OrderedDict([(key, result[key]) for key in result if result[key] is not None])
--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,17 @@ from rest_framework import routers
 
 from . import views
 
-router = routers.DefaultRouter()
+
+# class OptionalSlashRouter(routers.SimpleRouter):
+#
+#     def __init__(self):
+#         self.trailing_slash = '/?'
+#         super(routers.SimpleRouter, self).__init__()
+
+
+# router = OptionalSlashRouter()  # only who registered works
+router = routers.DefaultRouter(trailing_slash=True)
+# 'trailing_slash=True' no need ending in slash... oops, no working another day
 router.register(r'users', views.UserViewSet)
 router.register(r'todo', views.TodoViewSet)  # if here is not blank, then 'API Root' showed up.
 
@@ -12,7 +22,7 @@ urlpatterns = [
     path('', include('rest_auth.urls')),
     path('register/', include('rest_auth.registration.urls')),
 
-    # extends from 'viewsets.ModelViewSet', these two views is no needed.
+    # # extends from 'viewsets.ModelViewSet', these two views is no needed.
     # path('todo/', views.ListTodo.as_view()),
     # path('todo/<int:pk>/', views.Detail.as_view()),
 ]
